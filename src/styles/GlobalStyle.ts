5ae2a630d84ade11ef699db85fede708cec10d24
001salted <001salted@gmail.com>
2025-04-17 20:25:24 +0900

    🐛 fix/둥근모꼴 폰트 font-face 추가

--- src/styles/GlobalStyle.ts
+++ src/styles/GlobalStyle.ts
@@ -3,6 +3,13 @@ import { createGlobalStyle } from "styled-components";
 
 const GlobalStyle = createGlobalStyle`
 
+  @font-face {
+    font-family: 'DungGeunMo';
+    src: url('https://fastly.jsdelivr.net/gh/projectnoonnu/noonfonts_six@1.2/DungGeunMo.woff') format('woff');
+    font-weight: normal;
+    font-style: normal;
+  }
+
   body {
     font-family: 'Pretendard Variable', sans-serif;
     background-color: #F1F5EC;
