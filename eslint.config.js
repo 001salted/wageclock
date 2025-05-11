ced4e689489fa0972ff39c8e36fec29a35a04901
001salted <001salted@gmail.com>
2025-05-11 10:31:36 +0900

    🎨 style/airbnb룰 적용하도록 수정

--- eslint.config.js
+++ eslint.config.js
@@ -1,28 +1,54 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+import js from '@eslint/js';
+import prettier from 'eslint-config-prettier';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
-  { ignores: ['dist'] },
+  { ignores: ['dist', 'node_modules'] },
   {
     extends: [js.configs.recommended, ...tseslint.configs.recommended],
     files: ['**/*.{ts,tsx}'],
     languageOptions: {
-      ecmaVersion: 2020,
+      ecmaVersion: 'latest',
       globals: globals.browser,
     },
     plugins: {
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
+      react: react,
+      'jsx-a11y': jsxA11y,
+      import: importPlugin,
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
+      // Airbnb 스타일 룰 추가
+      'react/react-in-jsx-scope': 'off', // React 17+
+      'react/jsx-uses-react': 'off',
+      'react/jsx-filename-extension': ['warn', { extensions: ['.tsx'] }],
+      'react/function-component-definition': ['error', { namedComponents: 'arrow-function' }],
+      'react/prop-types': 'off',
+      'react/jsx-props-no-spreading': 'off',
+
+      'import/order': [
+        'error',
+        {
+          groups: ['builtin', 'external', 'internal', ['parent', 'sibling', 'index']],
+          'newlines-between': 'always',
+        },
       ],
+      'import/no-unresolved': 'off',
+
+      'jsx-a11y/anchor-is-valid': 'warn',
+      'jsx-a11y/no-static-element-interactions': 'warn',
+      'jsx-a11y/click-events-have-key-events': 'warn',
+      'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
     },
   },
-)
+  {
+    name: 'prettier',
+    plugins: {},
+    rules: prettier.rules,
+  },
+);
